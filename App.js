5db5aa67c941c00c2ea13ffe72ec69fb2d6c7391
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-12-01 16:53:59 +0300

    signOut a logged in user complete

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import HomeScreen from "./screens/HomeScreen";
+import { Button } from "@rneui/themed";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase";
 
 const Stack = createStackNavigator();
 
@@ -14,12 +17,28 @@ const globaScreenOptions = {
 };
 
 export default function App() {
+  const logOut = (navigation) => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigation.replace("Login");
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={globaScreenOptions}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ navigation, route }) => ({
+            headerRight: () => (
+              <Button onPress={() => logOut(navigation)} title="Log Out" />
+            ),
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { Button } from "@rneui/themed";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { StatusBar } from "expo-status-bar";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+
   return (
-    <View>
+    <SafeAreaView
+      options={{
+        // headerTitle: (props) => <LogoTitle {...props} />,
+        headerRight: () => (
+          <Button
+            onPress={logOut}
+            title="Log Out"
+            buttonStyle={{
+              borderRadius: 5,
+            }}
+          />
+        ),
+      }}
+    >
+      <StatusBar style="light" />
       <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
